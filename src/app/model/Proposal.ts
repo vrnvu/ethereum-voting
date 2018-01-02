5ee26a4e7a9122fa98c5ab4749e050b9edfe52b7
Arnau Diaz <diazarnau0@gmail.com>
2018-01-02 18:23:37 +0100

    Css tweaks

--- src/app/model/Proposal.ts
+++ src/app/model/Proposal.ts
@@ -4,3 +4,23 @@ export class Proposal {
   description: string;
   category: {id, name};
 }
+
+const cat0 = {
+  id: 0,
+  name: 'General'
+};
+
+const cat1 = {
+  id: 1,
+  name: 'Health'
+};
+
+const cat2 = {
+  id: 2,
+  name: 'Economy'
+};
+
+const cat3 = {
+  id: 3,
+  name: 'Environment'
+};
