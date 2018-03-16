a8efc5f0cf490360ddca7959c124851800aca9fd
Arnau Diaz <diazarnau0@gmail.com>
2018-03-16 16:02:03 +0100

    Bootstrap css preselection

---
+++ src/app/components/proposal-form/proposal-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-proposal-form',
+  templateUrl: './proposal-form.component.html',
+  styleUrls: ['./proposal-form.component.css']
+})
+export class ProposalFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
